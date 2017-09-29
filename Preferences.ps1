452c3ea69391d4e3ea7dfdd38fb4594682e481ed
Loïc MICHEL <loic.michel@gmail.com>
2017-09-29 16:27:58 +0200

    Initial import

---
+++ Preferences.ps1
@@ -0,0 +1,12 @@
+<# Adjust default variables if needed#>
+
+#Path
+$_TemplateRepository="C:\MyTemplates"
+
+#Email
+$_SMTPServer="smtpinterne.groupe.sa.colas.com"
+$_enc=[system.text.encoding]::UTF8 # encoding for mail
+
+#EventLog
+$_PRODSource="POWERSHELL_PROD"
+$_DEVSource="POWERSHELL_DEV"
