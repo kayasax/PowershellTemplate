d87c3339e5176a63c34f4df049fe053aa623a73e
Loïc MICHEL <loic.michel@gmail.com>
2017-10-01 13:28:05 +0200

    corrected mail encoding

--- Preferences.ps1
+++ Preferences.ps1
@@ -5,8 +5,8 @@ $_TemplateRepository= join-path "$_ScriptPath" -childpath "Templates"
 $_FunctionRepository=join-path "$_ScriptPath" -childpath "Functions"
 
 #Email
-$_SMTPServer="smtp.domain.com"
-$_enc=[system.text.encoding]::UTF8 # encoding for mail
+$_SMTPServer="localhost"
+$_enc='[system.text.encoding]::UTF8' # encoding for mail
 
 #EventLog
 $_PRODSource="POWERSHELL_PROD"
