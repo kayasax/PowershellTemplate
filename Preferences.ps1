827d4776162ef128846cbfc5d32c88f7f863e7f4
Loïc MICHEL <loic.michel@gmail.com>
2017-10-08 21:23:05 +0200

    Add error handling and email preferences

--- Preferences.ps1
+++ Preferences.ps1
@@ -4,8 +4,15 @@
 $_TemplateRepository= join-path "$_ScriptPath" -childpath "Templates"
 $_FunctionRepository=join-path "$_ScriptPath" -childpath "Functions"
 
+#Error Handling
+$_env="DEV" #choose the default environment (PROD or DEV)
+$_EmailAlert=$true
+$_EventLogAlert=$false # set to $true if you want to log the fatal error in Application EventLog
+
+
 #Email
 $_SMTPServer="localhost"
+$_to='user@domain.tld' # email recipients  you can use several addresses like this 'user1@dom.com','user2@dom.com'  
 $_enc='[system.text.encoding]::UTF8' # encoding for mail
 
 #EventLog
